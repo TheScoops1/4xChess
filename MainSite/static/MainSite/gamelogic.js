5109f7d57a53577aa3a5a071a50a91ff7233faff
TheScoops1 <scoopingether@gmail.com>
2025-01-03 13:29:54 -0600

    Fixed the possible moves queen logic, as well as added the king logic and began working on the knight logic

--- MainSite/static/MainSite/gamelogic.js
+++ MainSite/static/MainSite/gamelogic.js
@@ -207,13 +207,24 @@ function checkLegalMove(piece_being_moved, turn_marker) {
 function checkPossibleMoves(piece_to_check) {
   let possible_moves_to_return = []
   let current_location = piece_to_check.position
+
+  let new_x_position = current_location.x
+  let new_y_position = current_location.y
+
+  let new_right_diagonal_x_position = current_location.x
+  let new_right_diagonal_y_position = current_location.y
+
+  let new_left_diagonal_x_position = current_location.x
+  let new_left_diagonal_y_position = current_location.y
+
+
   if (piece_to_check.piece == "rook") {
     for (let i = 0; i < 8; i++) {
-      let new_x_position = current_location.x + 1
+      new_x_position = new_x_position + 1
       if (new_x_position > 7) {
         new_x_position = new_x_position - 8
       }
-      let new_y_position = current_location.y + 1
+      new_y_position = new_y_position + 1
       if (new_y_position > 7) {
         new_y_position = new_y_position - 8
       }
@@ -223,11 +234,11 @@ function checkPossibleMoves(piece_to_check) {
   }
   if (piece_to_check.piece == "bishop") {
     for (let i = 0; i < 8; i++) {
-      let new_right_diagonal_x_position = current_location.x + 1
-      let new_right_diagonal_y_position = current_location.y + 1
+      new_right_diagonal_x_position = new_right_diagonal_x_position - 1
+      new_right_diagonal_y_position = new_right_diagonal_y_position + 1
 
-      let new_left_diagonal_x_position = current_location.x - 1
-      let new_left_diagonal_y_position = current_location.y + 1
+      new_left_diagonal_x_position = new_left_diagonal_x_position + 1
+      new_left_diagonal_y_position = new_left_diagonal_y_position + 1
 
       if (new_right_diagonal_x_position > 7) {
         new_right_diagonal_x_position = new_right_diagonal_x_position - 8
@@ -276,20 +287,27 @@ function checkPossibleMoves(piece_to_check) {
         possible_moves_to_return.append({ x: current_location.x, y: new_y_position })
       }
     }
-    if (piece_to_check.piece == "queen") {
-      let new_right_diagonal_x_position = current_location.x + 1
-      let new_right_diagonal_y_position = current_location.y + 1
+  }
+  if (piece_to_check.piece == "queen") {
+    for (let i = 0; i < 8; i++) {
+      new_right_diagonal_x_position = new_right_diagonal_x_position - 1
+      new_right_diagonal_y_position = new_right_diagonal_y_position + 1
 
-      let new_left_diagonal_x_position = current_location.x - 1
-      let new_left_diagonal_y_position = current_location.y + 1
-      let new_x_position = current_location.x + 1
+      new_left_diagonal_x_position = new_left_diagonal_x_position + 1
+      new_left_diagonal_y_position = new_left_diagonal_y_position + 1
+
+      new_x_position = new_x_position + 1
+
       if (new_x_position > 7) {
         new_x_position = new_x_position - 8
       }
-      let new_y_position = current_location.y + 1
+
+      new_y_position = new_y_position + 1
+
       if (new_y_position > 7) {
         new_y_position = new_y_position - 8
       }
+
       possible_moves_to_return.append({ x: new_x_position, y: current_location.y })
       possible_moves_to_return.append({ x: current_location.x, y: new_y_position })
 
@@ -315,194 +333,215 @@ function checkPossibleMoves(piece_to_check) {
         }
       }
     }
-    if (piece_to_check.piece == "king") { }
-    if (piece_to_check.piece == "knight") { }
+  }
+  if (piece_to_check.piece == "king") {
+    let spot_one = { x: current_location.x + 1, y: current_location.y + 1 }
+    let spot_two = { x: current_location.x - 1, y: current_location.y + 1 }
+    let spot_three = { x: current_location.x - 1, y: current_location.y }
+    let spot_four = { x: current_location.x - 1, y: current_location.y - 1 }
+    let spot_five = { x: current_location.x, y: current_location.y - 1 }
+    let spot_six = { x: current_location.x + 1, y: current_location.y - 1 }
+    let spot_seven = { x: current_location.x + 1, y: current_location.y }
+    let spot_eight = { x: current_location.x + 1, y: current_location.y + 1 }
+
+    possible_moves_to_return.append(spot_one)
+    possible_moves_to_return.append(spot_two)
+    possible_moves_to_return.append(spot_three)
+    possible_moves_to_return.append(spot_four)
+    possible_moves_to_return.append(spot_five)
+    possible_moves_to_return.append(spot_six)
+    possible_moves_to_return.append(spot_seven)
+    possible_moves_to_return.append(spot_eight)
   }
 
-  function checkIfMoveIsOffBoard(piece_to_check, move_set) {
 
-  }
+  if (piece_to_check.piece == "knight") { }
+}
 
-  async function sendMoveToDB(session_token) {
-    try {
-      console.log(session_token);
-      console.log(game_board);
-      console.log(JSON.stringify(game_board))
-      const response = await fetch('/' + String(session_token) + '/test/',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': getCookie('csrftoken')
-          },
-          body: JSON.stringify(game_board)
-        });
+function checkIfMoveIsOffBoard(piece_to_check, move_set) {
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
+}
 
-      const data = await response.json();
-      console.log(data.test, ' Test was succesful');
-
-    } catch (error) {
-      console.log('Error:', error);
-    }
-  }
-
-  async function testGatherGameSession(session_token) {
-    try {
-      const response = await fetch('/' + String(session_token) + '/gather_game_session/', {
-        method: 'GET',
+async function sendMoveToDB(session_token) {
+  try {
+    console.log(session_token);
+    console.log(game_board);
+    console.log(JSON.stringify(game_board))
+    const response = await fetch('/' + String(session_token) + '/test/',
+      {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'X-CSRFToken': getCookie('csrftoken')
-        }
+        },
+        body: JSON.stringify(game_board)
       });
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
 
-      const data = await response.json();
-      console.log(data)
-    } catch (error) {
-      console.log('Error:', error);
+    const data = await response.json();
+    console.log(data.test, ' Test was succesful');
+
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+async function testGatherGameSession(session_token) {
+  try {
+    const response = await fetch('/' + String(session_token) + '/gather_game_session/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': getCookie('csrftoken')
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log(data)
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+async function createGameSession(session_token) {
+  try {
+    const response = await fetch('/' + String(session_token) + '/create_game_session/',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: JSON.stringify(game_pieces)
+      });
+    if (!response.ok) {
+      throw new Error('Network response was not okay');
+    }
+
+    const data = await response.json();
+    console.log(data)
+  } catch (error) {
+    console.log('Error:', error)
+  }
+}
+
+async function updateGameBoard(session_token) {
+  try {
+    const response = await fetch('/' + String(session_token) + "/update_game_board/",
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: JSON.stringify(game_pieces)
+      });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    console.log(data)
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+function determineLegalMove(name_of_piece, start_position, end_position, attack) {
+  game_piece = game_pieces[name_of_piece]
+  if (name_of_piece.includes("pawn")) {
+    if (attack) {
     }
   }
-
-  async function createGameSession(session_token) {
-    try {
-      const response = await fetch('/' + String(session_token) + '/create_game_session/',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': getCookie('csrftoken')
-          },
-          body: JSON.stringify(game_pieces)
-        });
-      if (!response.ok) {
-        throw new Error('Network response was not okay');
-      }
-
-      const data = await response.json();
-      console.log(data)
-    } catch (error) {
-      console.log('Error:', error)
-    }
-  }
-
-  async function updateGameBoard(session_token) {
-    try {
-      const response = await fetch('/' + String(session_token) + "/update_game_board/",
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': getCookie('csrftoken')
-          },
-          body: JSON.stringify(game_pieces)
-        });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      console.log(data)
-    } catch (error) {
-      console.log('Error:', error);
-    }
-  }
-
-  function determineLegalMove(name_of_piece, start_position, end_position, attack) {
-    game_piece = game_pieces[name_of_piece]
-    if (name_of_piece.includes("pawn")) {
-      if (attack) {
-      }
-    }
-  }
-  function pieceToMove(nameOfPiece, session_token) {
-    if (piece_to_move == null) {
-      piece_to_move = game_pieces[nameOfPiece]
-      if (turn_ident == true) {
-        if (piece_to_move.color == "white") {
-        } else {
-          piece_to_move = null
-        }
+}
+function pieceToMove(nameOfPiece, session_token) {
+  if (piece_to_move == null) {
+    piece_to_move = game_pieces[nameOfPiece]
+    if (turn_ident == true) {
+      if (piece_to_move.color == "white") {
       } else {
-        if (piece_to_move.color == "black") {
-        } else {
-          piece_to_move = null
-        }
+        piece_to_move = null
       }
     } else {
-      piece_to_attack = game_pieces[nameOfPiece]
-      console.log(piece_to_attack)
-      whereToMove({ class: "column_" + piece_to_attack.position.x + " row_" + piece_to_attack.position.y }, session_token)
+      if (piece_to_move.color == "black") {
+      } else {
+        piece_to_move = null
+      }
     }
+  } else {
+    piece_to_attack = game_pieces[nameOfPiece]
+    console.log(piece_to_attack)
+    whereToMove({ class: "column_" + piece_to_attack.position.x + " row_" + piece_to_attack.position.y }, session_token)
+  }
+}
+
+function whereToMove(spot_to_move_to, session_token) {
+
+  let piece_to_move_HTML_element = document.getElementsByClassName(piece_to_move.color + " " + piece_to_move.piece + " " + piece_to_move.piece_count)[0]
+  let spot_to_move_to_HTML_element = document.getElementsByClassName(spot_to_move_to.class)[0]
+
+  if (piece_to_attack == null) {
+  } else {
+    console.log(spot_to_move_to_HTML_element.firstElementChild)
+    spot_to_move_to_HTML_element.removeChild(spot_to_move_to_HTML_element.firstElementChild)
+
+    delete game_pieces[piece_to_attack.color + " " + piece_to_attack.piece + " " + piece_to_attack.piece_count]
   }
 
-  function whereToMove(spot_to_move_to, session_token) {
+  spot_to_move_to_HTML_element.append(piece_to_move_HTML_element)
+  spot_to_move_to_HTML_element.onclick = ''
 
-    let piece_to_move_HTML_element = document.getElementsByClassName(piece_to_move.color + " " + piece_to_move.piece + " " + piece_to_move.piece_count)[0]
-    let spot_to_move_to_HTML_element = document.getElementsByClassName(spot_to_move_to.class)[0]
+  let spot_moved_from_HTML_element = document.getElementsByClassName("column_" + piece_to_move.position.x + " row_" + piece_to_move.position.y)[0]
+  let class_name = spot_moved_from_HTML_element.className
 
-    if (piece_to_attack == null) {
-    } else {
-      console.log(spot_to_move_to_HTML_element.firstElementChild)
-      spot_to_move_to_HTML_element.removeChild(spot_to_move_to_HTML_element.firstElementChild)
+  let x_position_for_spot_moved_from = spot_moved_from_HTML_element.className[7]
+  let y_spot_for_spot_moved_from = spot_moved_from_HTML_element.className[13]
 
-      delete game_pieces[piece_to_attack.color + " " + piece_to_attack.piece + " " + piece_to_attack.piece_count]
-    }
+  spot_moved_from_HTML_element.onclick = function () { whereToMove({ 'class': class_name, 'x': Number(x_position_for_spot_moved_from), 'y': Number(y_spot_for_spot_moved_from) }, session_token) }
+  piece_to_move_HTML_element = null
 
-    spot_to_move_to_HTML_element.append(piece_to_move_HTML_element)
-    spot_to_move_to_HTML_element.onclick = ''
+  game_pieces[piece_to_move.color + " " + piece_to_move.piece + " " + piece_to_move.piece_count].position.x = spot_to_move_to.x
+  game_pieces[piece_to_move.color + " " + piece_to_move.piece + " " + piece_to_move.piece_count].position.y = spot_to_move_to.y
 
-    let spot_moved_from_HTML_element = document.getElementsByClassName("column_" + piece_to_move.position.x + " row_" + piece_to_move.position.y)[0]
-    let class_name = spot_moved_from_HTML_element.className
-
-    let x_position_for_spot_moved_from = spot_moved_from_HTML_element.className[7]
-    let y_spot_for_spot_moved_from = spot_moved_from_HTML_element.className[13]
-
-    spot_moved_from_HTML_element.onclick = function () { whereToMove({ 'class': class_name, 'x': Number(x_position_for_spot_moved_from), 'y': Number(y_spot_for_spot_moved_from) }, session_token) }
-    piece_to_move_HTML_element = null
-
-    game_pieces[piece_to_move.color + " " + piece_to_move.piece + " " + piece_to_move.piece_count].position.x = spot_to_move_to.x
-    game_pieces[piece_to_move.color + " " + piece_to_move.piece + " " + piece_to_move.piece_count].position.y = spot_to_move_to.y
-
-    piece_to_move = null
-    piece_to_attack = null
-    if (turn_ident == true) {
-      turn_ident = false
-    } else {
-      turn_ident = true
-    }
-
-    changeTitleHTML()
-
-    updateGameBoard(session_token)
-
+  piece_to_move = null
+  piece_to_attack = null
+  if (turn_ident == true) {
+    turn_ident = false
+  } else {
+    turn_ident = true
   }
 
-  function changeTitleHTML() {
-    turn_ident_HTML_element = document.getElementsByClassName("turn_ident")[0]
-    white_point_counter_HTML_element = document.getElementsByClassName("white_point_counter")[0]
-    black_point_counter_HTML_element = document.getElementsByClassName("black_point_counter")[0]
+  changeTitleHTML()
 
-    console.log(turn_ident_HTML_element)
-    console.log(white_point_counter_HTML_element)
-    console.log(black_point_counter_HTML_element)
+  updateGameBoard(session_token)
 
-    if (turn_ident == true) {
-      turn_ident_HTML_element.innerText = "Whites Turn"
-    } else {
-      turn_ident_HTML_element.innerText = "Blacks Turn"
-    }
+}
 
-    white_point_counter_HTML_element = String(whitePointCounter)
-    black_point_counter_HTML_element = String(blackPointCounter)
+function changeTitleHTML() {
+  turn_ident_HTML_element = document.getElementsByClassName("turn_ident")[0]
+  white_point_counter_HTML_element = document.getElementsByClassName("white_point_counter")[0]
+  black_point_counter_HTML_element = document.getElementsByClassName("black_point_counter")[0]
+
+  console.log(turn_ident_HTML_element)
+  console.log(white_point_counter_HTML_element)
+  console.log(black_point_counter_HTML_element)
+
+  if (turn_ident == true) {
+    turn_ident_HTML_element.innerText = "Whites Turn"
+  } else {
+    turn_ident_HTML_element.innerText = "Blacks Turn"
   }
+
+  white_point_counter_HTML_element = String(whitePointCounter)
+  black_point_counter_HTML_element = String(blackPointCounter)
+}
 
 
