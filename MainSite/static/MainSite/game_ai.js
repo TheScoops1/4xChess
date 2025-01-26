16c767482843e597c1f761d7bb686d77c8fab531
TheScoops1 <scoopingether@gmail.com>
2025-01-26 14:19:52 -0600

    adding the beginings of my node tree for the ai system

--- MainSite/static/MainSite/game_ai.js
+++ MainSite/static/MainSite/game_ai.js
@@ -1,16 +1,30 @@
 
 let data_tree = {}
 
-function createMoveTreNode(data) {
-    let node = new Object(data) {
-        const data = data;
-        const left = null;
-        const right = null;
-    }
-    return node
+function createMoveTreNode(data, parent_node_name) {
+  let node = new Object(data, parent_node_name) {
+    data = data,
+    name = parent_node_name,
+    left = null,
+    right = null
+  }
+  return node
 }
 
 function simulateGameMove(game_board, turn_indicator) {
+
+}
+
+function addNodeToTree(data, parent_node, direction) {
+
+  if (direction === 0) {
+    let new_node = createMoveTreNode(data, parent_node.name)
+  } else if (direction === 1) {
+    let new_node = createMoveTreNode(data, parent_node.name)
+  }
+
+  data_tree[new]
+
 
 }
 
